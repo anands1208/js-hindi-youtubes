e88f0aab285175d530836e2bec37e83bb2a71ebc
Anand mohan singh <109330423+anands1208@users.noreply.github.com>
2024-11-14 12:25:27 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log (3 
+    + 
+    3) // code redability should be high
+
+console.log("Anand")
+
+
+let name = "Anand"
+let age = 30
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log( typeof name )
